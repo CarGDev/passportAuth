628d11526c61b4dfb1e2bf26d6b13d0dc55c507a
cargdev <ingecarlos.gutierrez@gmail.com>
2020-10-11 17:32:22 -0500

    Oauth google

--- movies-api/routes/auth.js
+++ movies-api/routes/auth.js
@@ -9,7 +9,7 @@ const UserService = require('../services/users')
 const response = require('../network/response')
 const validationHandler = require('../utils/middleware/validationHandler')
 
-const { createUserSchema } = require('../utils/schemas/users')
+const { createUserSchema, createProviderUserSchema } = require('../utils/schemas/users')
 
 const { config } = require('../config')
 
@@ -69,8 +69,40 @@ const authApi = (app) => {
     }
   }
 
+  const routeSignProvider = async (req, res, next) => {
+    const { body } = req
+
+    const { apiKeyToken, ...user } = body
+    if (!apiKeyToken) next(boom.unauthorized('apiKeyToken required'))
+
+    try {
+      const querieUser = await userService.getOrCreateUser({ user })
+      const apiKey = await ApiKeysService.getApiKey({ token: apiKeyToken })
+
+      if (!apiKey) next(boom.unauthorized())
+
+      const { _id: id, name, email } = querieUser
+
+      const payload = {
+        sub: id,
+        name,
+        email,
+        scopes: apiKey.scopes
+      }
+
+      const token = jwt.sign(payload, config.authJwtSecret, {
+        expiresIn: '15m'
+      })
+
+      return response.success(req, res, token, { id, name, email }, 200)
+    } catch (error) {
+      next(error)
+    }
+  }
+
   router.post('/sign-in', routeAuth)
   router.post('/sign-up', validationHandler(createUserSchema), routeSignUP)
+  router.post('/sign-provider', validationHandler(createProviderUserSchema), routeSignProvider)
 }
 
 module.exports = authApi
